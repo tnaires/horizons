1bbe488abefb7b220ac15772845b011c49696c5c
Tarso Aires <tnaires@gmail.com>
2017-05-29 20:22:03 -0300

    Random header backgrounds are loaded each time the user refreshes the page

---
+++ assets/javascript/random_background.js
@@ -0,0 +1,12 @@
+---
+---
+{% assign background_images = site.static_files | where: "background", true %}
+let backgroundImages = [];
+
+{% for background_image in background_images %}
+  backgroundImages.push('{{ background_image.path }}');
+{% endfor %}
+
+let header = document.querySelector('header');
+let randomBackground = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+header.style.backgroundImage = `url(${randomBackground})`;
